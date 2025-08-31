fa1b748ee126c06f215811192c1d89ed0695e462
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2025-08-31 13:02:47 +0300

    Use RequestParam for interceptor passthrough

--- src/test/java/com/example/testsupport/framework/api/client/FrontApiClient.java
+++ src/test/java/com/example/testsupport/framework/api/client/FrontApiClient.java
@@ -2,14 +2,14 @@ package com.example.testsupport.framework.api.client;
 
 import com.example.testsupport.framework.api.client.params.GamblingBrandsParams;
 import com.example.testsupport.framework.api.dto.gambling.GamblingBrandsResponse;
-import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "frontApiClient", url = "${env.api.base-url}", configuration = GenericParamsInterceptor.class)
 public interface FrontApiClient {
 
     @GetMapping("/_front_api/api/v1/gambling/brands")
-    GamblingBrandsResponse getGamblingBrands(@Param("params") GamblingBrandsParams params);
+    GamblingBrandsResponse getGamblingBrands(@RequestParam("params") GamblingBrandsParams params);
 }
 
